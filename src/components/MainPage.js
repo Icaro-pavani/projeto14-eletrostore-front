46e867e8a777e5daf318266825d53b6a76c3743d
Icaro-pavani <icaro.pavani79@gmail.com>
2022-05-12 16:52:16 -0300

    feat: products card layout

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -3,8 +3,41 @@ import styled from "styled-components";
 import { IoMenuOutline, IoCartOutline, IoSearchSharp } from "react-icons/io5";
 
 import Header from "./Header";
+import ProductCard from "./ProductCard";
 
 export default function MainPage() {
+  const num = 2;
+  const product = [
+    {
+      name: `Smart TV LG AI ThinQ 50NANO75SPA LED 4K 50" 100V/240V`,
+      price: "3028.90",
+      amount: 5,
+      description: `A LG é inovação e isso pode ser visto em cada um dos seus produtos tecnológicos, especialmente pensados ​​para que toda a família aproveite mais a vida. Ter uma televisão LG é tirar proveito da mais alta qualidade do mercado.
+        Com Smart TV 50NANO75 você poderá acessar os aplicativos onde seu conteúdo favorito está localizado. Além disso, você pode navegar na web, interagir em redes sociais e se divertir com jogos de vídeo.
+        
+        Eu vivi em 4K
+        O número de pixels que oferece é 4 vezes maior do que Full HD, o resultado? Cenas muito mais realistas com um nível incrível de detalhes. Agora você vai conhecer uma aventura imersiva que não deixará de surpreendê-lo.
+        
+        Um som que o rodeia
+        Você sentirá que vem de todas as direções possíveis, o que enriquece a percepção disso. O diálogo de sua série de fim de semana ou a música de seus cantores favoritos assumirá um significado diferente.
+        
+        Além de assistir TV
+        Sua função Screen Share permite duplicar a tela do seu smartphone, tablet ou PC para aparecer na sua TV. Desta forma, você poderá visualizar todo tipo de conteúdo: material audiovisual, documentos de trabalho, e-mails e muito mais.`,
+      images: [
+        {
+          src: "https://http2.mlstatic.com/D_NQ_NP_678936-MLA47631790010_092021-O.webp",
+          alt: "Smart TV LG image 1",
+        },
+        {
+          src: "https://http2.mlstatic.com/D_NQ_NP_737407-MLA47631790012_092021-O.webp",
+          alt: "Smart TV LG image 2",
+        },
+      ],
+      categories: ["televisão"],
+      _id: "j4hjkh325h",
+    },
+  ];
+
   return (
     <MainPageContainer>
       <PageTop>
@@ -21,12 +54,24 @@ export default function MainPage() {
       <h2>Olá, User</h2>
       <h1>Do que você precisa hoje?</h1>
       <ProductsContainer>
-        <NavProducts>
+        <NavProducts num={num}>
           <p>Todos</p>
           <p>Notebooks</p>
           <p>Televisões</p>
           <p>Celulares</p>
         </NavProducts>
+        {product.length > 0 ? (
+          product.map((prod) => (
+            <ProductCard
+              title={prod.name}
+              price={prod.price}
+              image={prod.images[0]}
+              id={prod._id}
+            />
+          ))
+        ) : (
+          <h5>Não há produtos</h5>
+        )}
       </ProductsContainer>
     </MainPageContainer>
   );
@@ -40,6 +85,7 @@ const MainPageContainer = styled.div`
     line-height: 32px;
     font-weight: bold;
     margin-left: 25px;
+    margin-bottom: 40px;
   }
 
   h2 {
@@ -99,13 +145,17 @@ const SearchField = styled.div`
 const ProductsContainer = styled.div`
   width: 100%;
   background-color: var(--light-gray);
+  border-radius: 20px 20px 0 0;
 `;
 
 const NavProducts = styled.nav`
   display: flex;
   flex-wrap: nowrap;
   align-items: center;
-  margin-left: 25px;
+  margin-left: 35px;
+  margin-bottom: 20px;
+  padding-top: 30px;
+  overflow-x: auto;
 
   p {
     display: flex;
@@ -117,5 +167,12 @@ const NavProducts = styled.nav`
     font-size: 14px;
     line-height: 20px;
     border-radius: 30px;
+    letter-spacing: 0.2px;
+    padding: 15px;
+  }
+
+  p:nth-child(${(props) => props.num}) {
+    background-color: var(--blue);
+    color: #fff;
   }
 `;
---
+++ src/components/ProductCard.js
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export default function ProductCard({ title, price, image, id }) {
+  let exihibitTitle = "";
+  if (title.length > 30) {
+    exihibitTitle = title.slice(0, 31) + "...";
+  } else {
+    exihibitTitle = title;
+  }
+  return (
+    <ProductCardContainer>
+      <StyledLink to={`/products/${id}`}>
+        <img src={image.src} alt={image.alt} />
+        <h3>{exihibitTitle}</h3>
+        <h4>{`R$ ${price}`}</h4>
+      </StyledLink>
+    </ProductCardContainer>
+  );
+}
+
+const ProductCardContainer = styled.div`
+  width: 155px;
+  height: 245px;
+  padding: 15px;
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    width: 100%;
+    margin-bottom: 5px;
+  }
+
+  h3,
+  h4 {
+    color: var(--black);
+    font-size: 14px;
+    line-height: 18px;
+    letter-spacing: 0.2px;
+    margin-bottom: 7px;
+    text-align: center;
+  }
+
+  h4 {
+    font-weight: bold;
+  }
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: none;
+`;
